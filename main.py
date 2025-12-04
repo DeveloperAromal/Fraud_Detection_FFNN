75727ee947ca9733075d5f20a6884c2759d9fb32
DeveloperAromal <developeraromal@gmail.com>
2025-12-04 21:42:44 +0530

    created data processor

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-from utils.dataset_preprocessor import preprocess_csv
-
-if __name__ == "__main__":
-    input_csv = "data/raw/dataset.csv"
-    output_path = preprocess_csv(input_csv)
-
-    print("Processed file saved to:", output_path)
